01b4a29dc4a632528996d90980b5dceff0877d14
AlbertoCadorin <alberto.cadori01@gmail.com>
2025-03-07 15:36:46 +0100

    feat: snack 1 completed

--- js-snack1/app.js
+++ js-snack1/app.js
@@ -1,7 +1,8 @@
+// Creare un array di oggetti
 let bicycle = [
     {
         name : 'ragno',
-        weight : 23,
+        weight : 13,
     },
     {
         name : 'pluto',
@@ -16,8 +17,31 @@ let bicycle = [
     {
         name : 'lupo',
         weight : 25,
+    },
+    {
+        name : 'leone',
+        weight : 3,
     }
 ]
 
 console.log(bicycle)
 
+let bicycleWeight = bicycle[0]['weight'];
+console.log(bicycleWeight)
+let nameBicycle = bicycle[0];
+console.log(nameBicycle);
+//  ciclo per controllare la bici con il peso minore 
+for( let key in bicycle){
+
+    let control = (bicycle[key]['weight']);
+   // console.log(control);
+
+    const bicycleLowerWeight = (bicycle[key]);
+
+    if(control < bicycleWeight){
+        bicycleWeight = control;
+        nameBicycle = bicycleLowerWeight;
+    }
+}
+// stampoil nome e il peso della bici 
+console.log(`La bici con il peso minore è ${nameBicycle.name} e pesa ${bicycleWeight}kg`)
