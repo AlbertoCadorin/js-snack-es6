75508444144a002e5ab76520949b52c9f0b3bada
AlbertoCadorin <alberto.cadori01@gmail.com>
2025-03-07 16:43:09 +0100

    feat: snack 2 completed

--- js-snack2/app.js
+++ js-snack2/app.js
@@ -1,1 +1,53 @@
-console.log('looooooooooooooool')
+// un array di oggetti di squadre di calcio
+let teams =[
+    {
+        name : 'barcellona',
+        pointsMade : 0,
+        fouls : 0
+    },
+    {
+        name : 'juventus',
+        pointsMade : 0,
+        fouls : 0
+    },
+    {
+        name : 'milan',
+        pointsMade : 0,
+        fouls : 0
+    },
+    {
+        name : 'inter',
+        pointsMade : 0,
+        fouls : 0
+    },
+    {
+        name : 'real madrid',
+        pointsMade : 0,
+        fouls : 0
+    }
+]
+
+// Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”
+for( let key in teams){
+    teams[key]['pointsMade'] = randomNumber();
+    teams[key]['fouls'] = randomNumber();
+}
+console.log(teams);
+
+// Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console
+
+const secondariTeams = [];
+for(let key in teams){
+    secondariTeams.push({
+        neme : teams[key].name,
+        fouls : teams[key].fouls,
+    })
+}
+
+console.log(secondariTeams);
+
+// funzione numeri random
+function randomNumber() {
+    return Math.floor(Math.random() * 80) + 1;
+}
+
