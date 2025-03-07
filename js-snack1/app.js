4a9da1c408bbb50145979b7e9b9c4308cd85701e
AlbertoCadorin <alberto.cadori01@gmail.com>
2025-03-07 14:58:26 +0100

    feat: add object array.  snack1

--- js-snack1/app.js
+++ js-snack1/app.js
@@ -1,1 +1,23 @@
-console.log('ciaoooo')
+let bicycle = [
+    {
+        name : 'ragno',
+        weight : 23,
+    },
+    {
+        name : 'pluto',
+        weight :20,
+        
+    },
+    {
+        name : 'nitro',
+        weight : 18,
+
+    },
+    {
+        name : 'lupo',
+        weight : 25,
+    }
+]
+
+console.log(bicycle)
+
